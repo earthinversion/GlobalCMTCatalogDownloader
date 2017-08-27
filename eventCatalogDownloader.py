446673e95750b5e5c456cc7a9eee11177b1aadc8
Utpal Kumar <utpalkumar50@gmail.com>
2017-08-27 20:54:16 +0800

    bugs fixed

--- eventCatalogDownloader.py
+++ eventCatalogDownloader.py
@@ -3,98 +3,78 @@ import os
 import sys
 import numpy as np
 import re
-from datetime import datetime
+from datetime import datetime, date
 
 headerstr = '''This program gathers the event information from the Global CMT website
 http://www.globalcmt.org/CMTsearch.html
 Monthly catalogue is available for EQs from 1994-present.
 -Utpal Kumar
 ##############################################################'''
-print(headerstr, "\n")
+
 
 now = datetime.now()
 
-
 # Function for downloading the catalogue
-def catalogueDownload(year, month, filename):
+
+
+def catalogueDownload(year, month, loc):
     try:
+        if year < 1976:
+            print("Catalog file is not available")
+            sys.exit()
         yrsuffix = str(year)[-2:]
+        months = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec']
+        filename = months[int(month) - 1] + yrsuffix
+
         if 2005 <= year <= now.year:
-            url = 'http://www.ldeo.columbia.edu/~gcmt/projects/CMT/catalog/NEW_MONTHLY/%s/%s%s.ndk' % (str(year), month, yrsuffix)
+            url = 'http://www.ldeo.columbia.edu/~gcmt/projects/CMT/catalog/NEW_MONTHLY/%s/%s.ndk' % (str(year), filename)
+            ftype = ".ndk"
+            filename = filename + ftype
         elif 1994 < year <= 2004:
-            url = "http://www.ldeo.columbia.edu/~gcmt/projects/CMT/catalog/MONTHLY/%s%s.dek" % (month, yrsuffix)
+            url = "http://www.ldeo.columbia.edu/~gcmt/projects/CMT/catalog/MONTHLY/%s.dek" % (filename)
+            ftype = ".dek"
+            filename = filename + ftype
         elif year <= 1994:
             url = 'http://www.ldeo.columbia.edu/~gcmt/projects/CMT/catalog/jan76_dec04.dek'
-
-        if not os.path.exists(filename):
+            ftype = ".dek"
+            filename = "oldEQs" + ftype
+        fullfilename = loc + "/" + filename
+        if not os.path.isdir(loc):
+            os.makedirs(loc)
+        if not os.path.exists(fullfilename):
+            print("Downloading the catalog file...")
             r = requests.get(url)
-            with open(filename, "wb") as file:
+            with open(fullfilename, "wb") as file:
                 file.write(r.content)
-        else:
-            print("-->\tCatalogue file already exists!")
     except:
         print("ERROR downloading the data")
 
 
 if __name__ == "__main__":
+    print(headerstr, "\n")
     yearrange = input("Enter the year/month range(1994-present)\nFormat:'yyyy/mm-yyyy/mm'\ne.g. '2003/01-2004/12': ")
-
     # Checking for the input format
     rex = re.compile("^[0-9]{4}/[0-9]{2}-[0-9]{4}/[0-9]{2}$")
     if not rex.match(yearrange):
         print("Please enter the correct format")
         sys.exit()
 
-    try:
-        years1 = yearrange.split("-")
-        years2 = years1[0].split("/")
-        years3 = years1[1].split("/")
-        yearS = int(years2[0])
-        monS = int(years2[1])
-        yearE = int(years3[0])
-        monE = int(years3[1])
-        if yearS > yearE or now.year < yearE:
-            sys.exit()
-        elif yearS < 1976:
-            print("Catalogue file is not available")
-            sys.exit()
-        outdir = input("Enter the path to the download directory (else Enter for current directory): ")
-        if outdir == "":
-            outdir = "."
-        if not os.path.isdir(outdir):
-            os.makedirs(outdir)
-        months = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec']
-        for year in np.arange(yearS, yearE + 1):
-            if year > 1994:
-                # for starting year
-                if year == yearS:
-                    for month in months[monS - 1:]:
-                        print("Downloading the catalogue for %d/%s" % (year, month))
-                        filename = outdir + "/" + month + str(year)[-2:] + ".txt"
-                        catalogueDownload(year, month, filename)
+    years1 = yearrange.split("-")
+    yearS, monS = list(map(int, years1[0].split("/")))
+    yearE, monE = list(map(int, years1[1].split("/")))
 
-                # for ending year
-                elif year == yearE:
-                    for month in months[:monE]:
-                        print("Downloading the catalogue for %d/%s" % (year, month))
-                        filename = outdir + "/" + month + str(year)[-2:] + ".txt"
-                        catalogueDownload(year, month, filename)
-                # for other years
-                else:
-                    for month in months:
-                        print("Downloading the catalogue for %d/%s" % (year, month))
-                        filename = outdir + "/" + month + str(year)[-2:] + ".txt"
-                        catalogueDownload(year, month, filename)
+    if yearS > yearE or now.year < yearE:  # exit if the year entry is incorrect
+        print("Please enter proper format!")
+        sys.exit()
+    outdir = input("Enter the path to the download directory (else Enter for current directory): ")
+    if outdir == "":
+        outdir = "."
 
-            else:
-                print("Downloading the catalogue for 1976-2004")
-                filename = outdir + "/" + 'jan76_dec04' + ".txt"
-                catalogueDownload(year, 'none', filename)
-    except:
-        print("Please follow the proper format.")
-        if yearS < 1976:
-            print("Catalogue file is not available!")
-        elif yearS > yearE:
-            print("Starting/Ending year order doesn't seem to be correct!")
-        elif now.year < yearE:
-            print("Please enter available end year!")
+    startTime = date(yearS, monS, 1)
+    endTime = date(yearE, monE, 1)
+
+    ym_start = 12 * startTime.year + startTime.month - 1
+    ym_end = 12 * endTime.year + endTime.month
+    for ym in np.arange(ym_start, ym_end):
+        y, m = divmod(ym, 12)
+        catalogueDownload(y, m, outdir)
